f6d180df91560c1c6f3178ab33cc4a1d1a250cb3
alex0033 <chanchanmen0807@icloud.com>
2020-12-27 15:10:54 +0900

    refactoring for mydb_iofd.rb

--- iofd_test/mydb_iofd.rb
+++ iofd_test/mydb_iofd.rb
@@ -15,130 +15,155 @@ file_data_with_table = [{
     from: "iofd_test/file_data/#{table_name}"
 }]
 
-iofd "login as test_user" do |iofd|
-    iofd.directory_data_in_test = directory_data_at_first_login
-    iofd.io_contents = [
-        { output: "#{user_name}>", input: "exit" }
-    ]
-    iofd.directories = [user_dir]
-    iofd
+# このpart内にputsを置くことで見やすくできる余地あり
+def part(part_name)
+    yield
 end
 
-iofd "erorr command" do |iofd|
-    iofd.directory_data_in_test = directory_data_at_first_login
-    iofd.io_contents = [
-        { output: "#{user_name}>", input: "error command" },
-        { output: "そのコマンドは使えません" },
-        { output: "#{user_name}>", input: "exit" }
-    ]
-    iofd.directories = [user_dir]
-    iofd
+part "first" do
+    iofd "login as test_user" do |iofd|
+        iofd.directory_data_in_test = directory_data_at_first_login
+        iofd.io_contents = [
+            { output: "#{user_name}>", input: "exit" }
+        ]
+        iofd.directories = [user_dir]
+        iofd
+    end
+
+    iofd "erorr command" do |iofd|
+        iofd.directory_data_in_test = directory_data_at_first_login
+        iofd.io_contents = [
+            { output: "#{user_name}>", input: "error command" },
+            { output: "そのコマンドは使えません" },
+            { output: "#{user_name}>", input: "exit" }
+        ]
+        iofd.directories = [user_dir]
+        iofd
+    end    
 end
 
-iofd "create database database_name" do |iofd|
-    iofd.directory_data_in_test = directory_data_logged_in
-    iofd.io_contents = [
-        { output: "#{user_name}>", input: "create database #{database_name}" },
-        { output: "#{user_name}>", input: "exit" }
-    ]
-    iofd.directories = ["#{user_dir}/#{database_name}"]
-    iofd
+part "create" do
+    part "create database" do
+        iofd "create database database_name" do |iofd|
+            iofd.directory_data_in_test = directory_data_logged_in
+            iofd.io_contents = [
+                { output: "#{user_name}>", input: "create database #{database_name}" },
+                { output: "#{user_name}>", input: "exit" }
+            ]
+            iofd.directories = ["#{user_dir}/#{database_name}"]
+            iofd
+        end
+
+        iofd "create database database_name again" do |iofd|
+            iofd.directory_data_in_test = directory_data_with_database
+            iofd.io_contents = [
+                { output: "#{user_name}>", input: "create database #{database_name}" },
+                { output: "\"create\"以後の書き方に誤りがあります。", input: nil },
+                { output: "#{user_name}", input: "exit" }
+            ]
+            iofd.directories = ["#{user_dir}/#{database_name}"]
+            iofd
+        end
+    end
+ 
+    part "create table" do
+        iofd "create table table_name" do |iofd|
+            iofd.directory_data_in_test = directory_data_with_database
+            iofd.io_contents = [
+                { output: "#{user_name}>", input: "use #{database_name}" },
+                { output: "#{user_name}>#{database_name}>", input: "create table #{table_name}" },
+                { output: "#{user_name}>#{database_name}>", input: "exit" }
+            ]
+            iofd.files = [{ 
+                original: "#{user_dir}/#{database_name}/#{table_name}.csv",
+                comparison: "/iofd_test/comparison_files/#{table_name}"
+            }]
+            iofd
+        end
+    end
+
+    part "create another" do
+        iofd "create miss_spell aaa" do |iofd|
+            iofd.directory_data_in_test = directory_data_at_first_login
+            iofd.io_contents = [
+                { output: "#{user_name}>", input: "create miss_spell aaa" },
+                { output: "\"create\"以後の書き方に誤りがあります。", input: nil },
+                { output: "#{user_name}", input: "exit" }
+            ]
+            iofd
+        end   
+    end
 end
 
-iofd "use database_name" do |iofd|
-    iofd.directory_data_in_test = directory_data_with_database
-    iofd.io_contents = [
-        { output: "#{user_name}>", input: "use #{database_name}" },
-        { output: "#{user_name}>#{database_name}>", input: "exit" }
-    ]
-    iofd.directories = ["#{user_dir}/#{database_name}"]
-    iofd
+part "drop" do
+    part "drop database" do
+        iofd "drop database database_name when use database_name" do |iofd|
+            iofd.directory_data_in_test = directory_data_with_database
+            iofd.io_contents = [
+                { output: "#{user_name}>", input: "use #{database_name}" },
+                { output: "#{user_name}>#{database_name}>", input: "drop database #{database_name}" },
+                { output: "#{user_name}", input: "exit" }
+            ]
+            iofd.remove_directories = ["#{user_dir}/#{database_name}"]
+            iofd
+        end
+        
+        iofd "drop database database_name when not use database" do |iofd|
+            iofd.directory_data_in_test = directory_data_with_database
+            iofd.io_contents = [
+                { output: "#{user_name}>", input: "drop database #{database_name}" },
+                { output: "#{user_name}", input: "exit" }
+            ]
+            iofd.remove_directories = ["#{user_dir}/#{database_name}"]
+            iofd
+        end
+        
+        iofd "drop database database_name when database_name not exist" do |iofd|
+            iofd.directory_data_in_test = directory_data_at_first_login
+            iofd.io_contents = [
+                { output: "#{user_name}>", input: "drop database #{database_name}" },
+                { output: "\"drop\"以後の書き方に誤りがあります。" },
+                { output: "#{user_name}", input: "exit" }
+            ]
+            iofd
+        end
+    end
+ 
+    part "drop table" do
+        iofd "drop table table_name" do |iofd|
+            iofd.directory_data_in_test = directory_data_with_database
+            iofd.file_data_in_test = file_data_with_table
+            iofd.io_contents = [
+                { output: "#{user_name}>", input: "use #{database_name}" },
+                { output: "#{user_name}>#{database_name}>", input: "drop table #{table_name}" },
+                { output: "#{user_name}>#{database_name}>", input: "exit" }
+            ]
+            iofd.remove_files = ["#{user_dir}/#{database_name}/#{table_name}.csv"]
+            iofd
+        end        
+    end
+
+    part "drop another" do
+        iofd "drop miss_spell aaa" do |iofd|
+            iofd.directory_data_in_test = directory_data_at_first_login
+            iofd.io_contents = [
+                { output: "#{user_name}>", input: "drop miss_spell aaa" },
+                { output: "\"drop\"以後の書き方に誤りがあります。" },
+                { output: "#{user_name}", input: "exit" }
+            ]
+            iofd
+        end        
+    end
 end
 
-iofd "create table table_name" do |iofd|
-    iofd.directory_data_in_test = directory_data_with_database
-    iofd.io_contents = [
-        { output: "#{user_name}>", input: "use #{database_name}" },
-        { output: "#{user_name}>#{database_name}>", input: "create table #{table_name}" },
-        { output: "#{user_name}>#{database_name}>", input: "exit" }
-    ]
-    iofd.files = [{ 
-        original: "#{user_dir}/#{database_name}/#{table_name}.csv",
-        comparison: "/iofd_test/comparison_files/#{table_name}"
-    }]
-    iofd
-end
-
-iofd "drop database database_name when use database_name" do |iofd|
-    iofd.directory_data_in_test = directory_data_with_database
-    iofd.io_contents = [
-        { output: "#{user_name}>", input: "use #{database_name}" },
-        { output: "#{user_name}>#{database_name}>", input: "drop database #{database_name}" },
-        { output: "#{user_name}", input: "exit" }
-    ]
-    iofd.remove_directories = ["#{user_dir}/#{database_name}"]
-    iofd
-end
-
-iofd "drop database database_name when not use database" do |iofd|
-    iofd.directory_data_in_test = directory_data_with_database
-    iofd.io_contents = [
-        { output: "#{user_name}>", input: "drop database #{database_name}" },
-        { output: "#{user_name}", input: "exit" }
-    ]
-    iofd.remove_directories = ["#{user_dir}/#{database_name}"]
-    iofd
-end
-
-iofd "drop table table_name" do |iofd|
-    iofd.directory_data_in_test = directory_data_with_database
-    iofd.file_data_in_test = file_data_with_table
-    iofd.io_contents = [
-        { output: "#{user_name}>", input: "use #{database_name}" },
-        { output: "#{user_name}>#{database_name}>", input: "drop table #{table_name}" },
-        { output: "#{user_name}>#{database_name}>", input: "exit" }
-    ]
-    iofd.remove_files = ["#{user_dir}/#{database_name}/#{table_name}.csv"]
-    iofd
-end
-
-iofd "create miss_spell aaa" do |iofd|
-    iofd.directory_data_in_test = directory_data_at_first_login
-    iofd.io_contents = [
-        { output: "#{user_name}>", input: "create miss_spell aaa" },
-        { output: "\"create\"以後の書き方に誤りがあります。", input: nil },
-        { output: "#{user_name}", input: "exit" }
-    ]
-    iofd
-end
-
-iofd "create database database_name again" do |iofd|
-    iofd.directory_data_in_test = directory_data_with_database
-    iofd.io_contents = [
-        { output: "#{user_name}>", input: "create database #{database_name}" },
-        { output: "\"create\"以後の書き方に誤りがあります。", input: nil },
-        { output: "#{user_name}", input: "exit" }
-    ]
-    iofd.directories = ["#{user_dir}/#{database_name}"]
-    iofd
-end
-
-iofd "drop miss_spell aaa" do |iofd|
-    iofd.directory_data_in_test = directory_data_at_first_login
-    iofd.io_contents = [
-        { output: "#{user_name}>", input: "drop miss_spell aaa" },
-        { output: "\"drop\"以後の書き方に誤りがあります。" },
-        { output: "#{user_name}", input: "exit" }
-    ]
-    iofd
-end
-
-iofd "drop database database_name when database_name not exist" do |iofd|
-    iofd.directory_data_in_test = directory_data_at_first_login
-    iofd.io_contents = [
-        { output: "#{user_name}>", input: "drop database #{database_name}" },
-        { output: "\"drop\"以後の書き方に誤りがあります。" },
-        { output: "#{user_name}", input: "exit" }
-    ]
-    iofd
+part "use" do
+    iofd "use database_name" do |iofd|
+        iofd.directory_data_in_test = directory_data_with_database
+        iofd.io_contents = [
+            { output: "#{user_name}>", input: "use #{database_name}" },
+            { output: "#{user_name}>#{database_name}>", input: "exit" }
+        ]
+        iofd.directories = ["#{user_dir}/#{database_name}"]
+        iofd
+    end    
 end
